90ea5151b8e730e5eb2ac128218b7acbe781f958
Yuriy Petrushenko <akeno7@mail.ru>
2023-02-10 16:30:47 +0700

    Add a check for intersections

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,15 +13,11 @@ struct circle {
     struct point point;
     double r;
 };
-/* void AddCirlce(struct circle* circle, int n)
-{
-    circle = (struct circle*)realloc(struct circle, sizeof());
-}
- */
+
 int main()
 {
     int i = 1;
-    int n;
+    int n, SR, D;
     double P, S;
     char str[100];
     FILE* data;
@@ -38,7 +35,8 @@ int main()
         i++;
     }
     for (int i = 0; i < n; i++) {
-        printf("circle(%.2lf %.2lf, %.2lf) \n",
+        printf(" %d. circle(%.2lf %.2lf, %.2lf) \n",
+               i + 1,
                circle[i].point.x,
                circle[i].point.y,
                circle[i].r);
@@ -46,6 +44,17 @@ int main()
         S = pi * circle[i].r * circle[i].r;
         printf("\tperimetr = %f\n", P);
         printf("\tarea = %f\n", S);
+        printf("\tintersects: \n");
+        for (int j = 0; j < n - 1; j++) {
+            if (i == j)
+                continue;
+            SR = circle[j].r + circle[j + 1].r;
+            D
+                    = sqrt(pow(circle[i].point.x - circle[j].point.x, 2)
+                           + pow(circle[i].point.y - circle[j].point.y, 2));
+            if (SR > D)
+                printf("\t  %d. circle \n", j + 1);
+        }
         printf("\n");
     }
     return 0;
