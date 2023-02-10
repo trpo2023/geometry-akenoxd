0ce67441a898b524dbc4ab1a5ed2bdb66e832083
Yuriy Petrushenko <akeno7@mail.ru>
2023-02-10 16:11:35 +0700

    output area and perimetr

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define pi 3.14159
 
 struct point {
     double x;
@@ -20,6 +21,7 @@ int main()
 {
     int i = 1;
     int n;
+    double P, S;
     char str[100];
     FILE* data;
     data = fopen("example", "r");
@@ -33,14 +35,18 @@ int main()
         circle[i - 1].point.x = strtod(strchr(str, '(') + 1, NULL);
         circle[i - 1].point.y = strtod(strchr(str, ' ') + 1, NULL);
         circle[i - 1].r = strtod(strchr(str, ',') + 1, NULL);
-        puts(str);
         i++;
     }
     for (int i = 0; i < n; i++) {
-        printf("%.2lf %.2lf, %.2lf \n",
+        printf("circle(%.2lf %.2lf, %.2lf) \n",
                circle[i].point.x,
                circle[i].point.y,
                circle[i].r);
+        P = 2 * pi * circle[i].r;
+        S = pi * circle[i].r * circle[i].r;
+        printf("\tperimetr = %f\n", P);
+        printf("\tarea = %f\n", S);
+        printf("\n");
     }
     return 0;
     free(circle);
