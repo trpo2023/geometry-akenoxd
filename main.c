e0740613c100c406935c63d9476b8de79bae823a
Yuriy Petrushenko <akeno7@mail.ru>
2023-02-10 15:39:50 +0700

    Input the circles

--- main.c
+++ main.c
@@ -11,10 +11,15 @@ struct circle {
     struct point point;
     double r;
 };
-
+/* void AddCirlce(struct circle* circle, int n)
+{
+    circle = (struct circle*)realloc(struct circle, sizeof());
+}
+ */
 int main()
 {
-    double n;
+    int i = 1;
+    int n;
     char str[100];
     FILE* data;
     data = fopen("example", "r");
@@ -22,10 +27,21 @@ int main()
         printf("Error opening file");
         return 1;
     }
+    fscanf(data, "%d\n", &n);
+    struct circle* circle = malloc(sizeof(*circle) * n);
     while (fgets(str, 100, data) != NULL) {
-        n = strtod(strchr(str, '(') + 1, NULL);
-        printf("%.2f \n", n);
+        circle[i - 1].point.x = strtod(strchr(str, '(') + 1, NULL);
+        circle[i - 1].point.y = strtod(strchr(str, ' ') + 1, NULL);
+        circle[i - 1].r = strtod(strchr(str, ',') + 1, NULL);
         puts(str);
+        i++;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%.2lf %.2lf, %.2lf \n",
+               circle[i].point.x,
+               circle[i].point.y,
+               circle[i].r);
     }
     return 0;
+    free(circle);
 }
