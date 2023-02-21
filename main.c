482de88850eceb27be9240b51266e6b493bc695d
Yuriy Petrushenko <akeno7@mail.ru>
2023-02-21 16:01:40 +0700

    Removed unused code for triangles and polygons

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define pi 3.14159
 
 struct point {
     double x;
@@ -15,15 +14,11 @@ struct circle {
     double r;
 };
 
-struct triangle {
-    struct point A;
-    struct point B;
-    struct point C;
-};
 int Check_Error(char* str)
 {
     if (!(strncmp("circle", str, 6))) {
-        if (!(isdigit(*(strchr(str, '(') + 1)))) {
+        if (!(isdigit(*(strchr(str, '(') + 1)))
+            && !(*(strchr(str, '(') + 1) = '-')) {
             printf("%s", str);
             for (int i = 0; i < strchr(str, '(') - &str[0] + 1; i++)
                 printf(" ");
@@ -43,14 +38,9 @@ int Check_Error(char* str)
             return 0;
         } else
             return 1;
-    } else if (!(strncmp("triangle", str, 8))) {
-        return 2;
-    } else if (!(strncmp("polygon", str, 7))) {
-        return 3;
     } else {
         printf("%s \n^\n", str);
-        printf("Eror at column 0: expected \'circle\', \'triangle\' or "
-               "\'polygon\'\n\n");
+        printf("Eror at column 0: expected \'circle\'\n\n");
         return 0;
     }
 }
@@ -58,9 +48,8 @@ int Check_Error(char* str)
 int main(int argc, char** argv)
 {
     struct circle* circle;
-    struct triangle* triangle;
-    int nCircle = 0, nTriangle = 0, nPolygon = 0;
-    int n = 0, code;
+    int nCircle = 0;
+    int code;
     int SR, D;
     double P, S;
     char str[100];
@@ -71,43 +60,15 @@ int main(int argc, char** argv)
     }
 
     while (fgets(str, 100, data) != NULL) {
-        n = nCircle + nTriangle + nPolygon;
         code = Check_Error(str);
         if (!code)
             continue;
-        if (code == 1) {
+        if (code) {
             nCircle++;
             circle = realloc(circle, nCircle * (sizeof(struct circle)));
             circle[nCircle - 1].point.x = strtod(strchr(str, '(') + 1, NULL);
             circle[nCircle - 1].point.y = strtod(strchr(str, ' ') + 1, NULL);
             circle[nCircle - 1].r = strtod(strchr(str, ',') + 1, NULL);
-        } else if (code == 2) {
-            nTriangle++;
-            triangle = realloc(triangle, nTriangle * (sizeof(struct triangle)));
-            triangle[nTriangle - 1].A.x = strtod(strchr(str, '(') + 2, NULL);
-            triangle[nTriangle - 1].A.y = strtod(strchr(str, ' ') + 1, NULL);
-            for (int i = 0; i < strchr(str, ',') - str; i++) {
-                str[i] = ' ';
-                n = i;
-            }
-            str[n + 1] = ' ';
-            triangle[nTriangle - 1].B.x = strtod(strchr(str, ',') + 1, NULL);
-            triangle[nTriangle - 1].B.y = strtod(strchr(str, ' ') + 1, NULL);
-            for (int i = 0; i < strchr(str, ',') - &str[0]; i++) {
-                str[i] = ' ';
-                n = i;
-            }
-            str[n + 1] = ' ';
-            triangle[nTriangle - 1].C.x = strtod(strchr(str, ',') + 1, NULL);
-            triangle[nTriangle - 1].C.y = strtod(strchr(str, ' ') + 1, NULL);
-        } else if (code == 3) {
-            nPolygon++;
-            continue;
-        } else {
-            printf("%s \n^\n", str);
-            printf("Eror at column 0: expected \'circle\', \'triangle\' or "
-                   "\'polygon\'\n\n");
-            continue;
         }
     }
     printf("\n");
@@ -117,8 +78,8 @@ int main(int argc, char** argv)
                circle[i].point.x,
                circle[i].point.y,
                circle[i].r);
-        P = 2 * pi * circle[i].r;
-        S = pi * circle[i].r * circle[i].r;
+        P = 2 * M_PI * circle[i].r;
+        S = M_PI * circle[i].r * circle[i].r;
         printf("\tperimetr = %f\n", P);
         printf("\tarea = %f\n", S);
         printf("\tintersects: \n");
@@ -134,17 +95,6 @@ int main(int argc, char** argv)
         }
         printf("\n");
     }
-    for (int i = 0; i < nTriangle; i++) {
-        printf(" %d. triangle(%.2lf %.2lf, %.2lf %.2lf, %.2lf %.2lf) \n",
-               i + 1,
-               triangle[i].A.x,
-               triangle[i].A.y,
-               triangle[i].B.x,
-               triangle[i].B.y,
-               triangle[i].C.x,
-               triangle[i].C.y);
-    }
     free(circle);
-    free(triangle);
     return 0;
 }
